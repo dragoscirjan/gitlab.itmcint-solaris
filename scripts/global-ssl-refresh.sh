511d6d75976e16dbdb88fcac3cafd72543c9675d
Dragos Cirjan <dragos.cirjan@itmediaconnect.ro>
2018-05-30 00:56:43 +0300

    new data for ssl

--- scripts/global-ssl-refresh.sh
+++ scripts/global-ssl-refresh.sh
@@ -28,7 +28,7 @@ punctart.ro www.punctart.ro
 # Dragos Cirjan
 # "lunaticthinker.me"
 # IT Media Connect
-itmcd.ro dragosc.itmcd.ro www.dragosc.itmcd.ro syrius.itmcd.ro www.syrius.itmcd.ro www.itmcd.ro
+itmcd.ro dragosc.itmcd.ro www.dragosc.itmcd.ro galatea.itmcd.ro www.galatea.itmcd.ro syrius.itmcd.ro www.syrius.itmcd.ro www.itmcd.ro
 DOMAINS
 
 cat $DOMAIN_FILE | grep -v "#" | while read DOMAIN; do
