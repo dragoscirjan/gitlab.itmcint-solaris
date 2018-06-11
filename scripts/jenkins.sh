2d0d6cc3d47484cce495480d715a12f1a09b0fd2
Dragos Cirjan <dragos.cirjan@itmediaconnect.ro>
2018-06-11 22:23:33 +0300

    +

--- scripts/jenkins.sh
+++ scripts/jenkins.sh
@@ -6,21 +6,21 @@ JENKINS_WAR=${JENKINS_WAR:-/opt/jenkins.war}
 
 [ -f $JENKINS_WAR ] || curl -SL http://mirrors.jenkins.io/war/latest/jenkins.war > $JENKINS_WAR
 
-USER=${USER:-jenkins}
+JENKINS_USER=${JENKINS_USER:-jenkins}
 
-cat /etc/passwd | grep $USER || (
-	groupadd -g 12345 $USER
-	useradd -u 12345 -g $USER -s /bin/bash $USER
+cat /etc/passwd | grep $JENKINS_USER || (
+	groupadd -g 12345 $JENKINS_USER
+	useradd -u 12345 -g $JENKINS_USER -s /bin/bash $JENKINS_USER
 )
 
 JENKINS_HOME=${JENKINS_HOME:-/var/lib/jenkins}
 JENKINS_PID=${JENKINS_PID:-/var/run/jenkins.pid}
 
 touch $JENKINS_PID
-chown $USER:$USER $JENKINS_PID
+chown $JENKINS_USER:$JENKINS_USER $JENKINS_PID
 
 mkdir -p $JENKINS_HOME
-chown -R $USER:$USER $JENKINS_HOME
+chown -R $JENKINS_USER:$JENKINS_USER $JENKINS_HOME
 
 [ -f $JENKINS_PID ] && [ "$(cat $JENKINS_PID)" != "" ] && kill -s 9 $(cat $JENKINS_PID)
 
