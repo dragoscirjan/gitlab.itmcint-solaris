f3bf7d0977ea67173b6f1a4a9f598f528a692015
Dragos Cirjan <dragos.cirjan@itmediaconnect.ro>
2018-06-11 23:05:24 +0300

    +

--- scripts/jenkins.sh
+++ scripts/jenkins.sh
@@ -14,7 +14,7 @@ cat /etc/passwd | grep $JENKINS_USER || (
 	useradd -u 12345 -g $JENKINS_USER -s /bin/bash $JENKINS_USER
 )
 
-JENKINS_HOME=${JENKINS_HOME:-/var/lib/jenkins}
+export JENKINS_HOME=${JENKINS_HOME:-/var/lib/jenkins}
 JENKINS_LOG=${JENKINS_LOG:-/var/log/jenkins}
 JENKINS_PID=${JENKINS_PID:-/var/run/jenkins.pid}
 
@@ -27,5 +27,5 @@ chown -R $JENKINS_USER:$JENKINS_USER $JENKINS_HOME $JENKINS_LOG
 # [ -f $JENKINS_PID ] && [ "$(cat $JENKINS_PID)" != "" ] && kill -s 9 $(cat $JENKINS_PID)
 
 su -s /bin/sh jenkins \
-    -c "exec setsid /usr/bin/java -jar $JENKINS_WAR $JENKINS_OPTS </dev/null >> $JENKINS_LOG/console_log 2>&1 & 
-echo \$! > $JENKINS_PID "
+    -c "exec setsid /usr/bin/java -jar $JENKINS_WAR $JENKINS_OPTS </dev/null >>`$JENKINS_LOG/console_log 2>&1 & 
+echo \$! >$JENKINS_PID "
